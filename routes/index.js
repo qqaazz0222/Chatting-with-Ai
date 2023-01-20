1237958807630659c111903f20bbae3b8182693a
qqaazz0222 <qqaazz0222@naver.com>
2023-01-20 13:51:32 +0900

    개발 / 최초 커밋

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
+// import { getCompletion } from "gpt3";
+const router = express.Router();
+
+const configuration = new Configuration({
+    apikey: "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",
+});
+// apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAIApi(configuration);
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async (req, res) => {
+    res.render("index", { title: "Express" });
+});
+
+router.get("/test", async (req, res) => {
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: "hello",
+        temperature: 0.7,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+    // console.log();
+    res.render("index", { title: "Express" });
 });
 
 module.exports = router;
